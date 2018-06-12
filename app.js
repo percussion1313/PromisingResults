d769e0d6752c3f8ba87c548c3c437d1cf08891f8
percussion1313 <peterjacobgrant@gmail.com>
2018-06-12 16:03:48 -0500

    Completed promisechain and played around with numbers

---
+++ app.js
@@ -0,0 +1,44 @@
+console.log('test');
+
+//First version:
+/*slowMath.add(6, 2)
+.then((add) => {
+    console.log(result);
+}).then(slowMath.multiply)*/
+
+slowMath.add(1, 1)
+    .then(function(result) {
+    console.log(result);
+slowMath.multiply(result, 2)
+    .then(function (result) {
+    console.log(result);
+slowMath.subtract(result, 3)
+    .then(function (result) {
+    console.log(result);
+slowMath.add(result, 98)
+    .then(function (result) {
+    console.log(result);
+slowMath.remainder(result, 2)
+    .then(function (result) {
+    console.log(result);
+slowMath.multiply(result, 50)
+    .then(function (result) {
+    console.log(result);
+slowMath.remainder(result, 40)
+    .then(function (result) {
+    console.log(result);
+slowMath.add(result, 32)
+    .then(function (result) {
+    console.log(result) 
+    setTimeout (function() { 
+    console.log(`The final result is ${result}`)}, 1000)
+}).catch(function(err) {
+    console.log(err)
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
